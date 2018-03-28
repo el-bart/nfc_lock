a05f8cc21c91c09ebedc11041e704bec613e9f4c
bartek 'basz' szurgot <git@baszerr.eu>
2018-03-28 22:32:51 +0200

    small cleanup of apps + added target to flash created binaries

--- src/stm8/cmake/config.cmake
+++ src/stm8/cmake/config.cmake
@@ -1,31 +1,18 @@
-add_definitions(-D STM8S003)
+# final platform:
+#add_definitions(-D STM8S003)
+#set(MCU_TYPE "stmxxx")
 
-include_directories(${CMAKE_SOURCE_DIR}/Stm8sStdPeriph)
+# evaluation board:
+add_definitions(-D STM8S105)
+set(MCU_TYPE "stm8s105c6")
 
-#set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=gnu99")
-
-#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
-#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-rtti -fno-exceptions")
-
-#add_definitions(-mthumb -mcpu=cortex-m0)
-#add_definitions(-nostartfiles)
-
-#set(LINKER_SCRIPT ${CMAKE_SOURCE_DIR}/arch/stm32/stm32f0_linker.ld)
-#set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -T${LINKER_SCRIPT}")
-#set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -mthumb -mcpu=cortex-m0 ")
-#set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -nostartfiles")
-
-#enable_language(ASM)
+include_directories(${CMAKE_SOURCE_DIR}/st)
 
 function(add_binary BINNAME)
-  add_executable(${BINNAME}.out ${ARGN})
-  #target_link_libraries(${BINNAME}.out)
-  add_custom_command(OUTPUT ${BINNAME}.bin
-      COMMAND arm-none-eabi-objcopy -O binary ${BINNAME}.out ${BINNAME}.bin
-      DEPENDS ${BINNAME}.out
-      COMMENT "OBJCOPY ${BINNAME}.bin"
-    )
-  add_custom_target(${BINNAME} ALL
-      DEPENDS ${BINNAME}.out ${BINNAME}.bin
+  add_executable(${BINNAME} ${ARGN})
+  add_custom_target(${BINNAME}-flash
+      COMMAND stm8flash -c stlink -p ${MCU_TYPE} -w ${BINNAME}.ihx
+      DEPENDS ${BINNAME}
+      COMMENT "FLASH ${BINNAME}.ihx"
     )
 endfunction()
